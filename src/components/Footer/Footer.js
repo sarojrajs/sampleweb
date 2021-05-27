9136f6cb34e4297781e41f17911efefd2f61582a
unknown <sarojrajs206@gmail.com>
2021-05-27 20:24:09 +0530

    Commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Footer.css'
-import img from '../../images/Group 19.png'
+
 function Footer() {
     return (
         <div className='footer'>
-            <img src={img} alt=''/>
+           
         </div>
     )
 }
